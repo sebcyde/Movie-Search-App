ba8bfcf073a212681da2a06059e14d93435a4f83
Seb <sebcyde@hotmail.com>
2021-09-18 22:15:55 +0100

    finished dom manipulation post search need to add search image source and integrate API content

--- js/apis.js
+++ js/apis.js
@@ -1,8 +1,23 @@
 export function global() {
 
 
+    const createLi = document.createElement('li');
 
-    document.querySelector('.searchsubmit').addEventListener('click', (e) => {
+    const changeImg = document.createElement('img');
+
+    const createDiv = document.createElement('div');
+
+    const createH3 = document.createElement('h3');
+
+    const createH4Year = document.createElement('h4');
+
+    const createH4Type = document.createElement('h4');
+
+    const createP = document.createElement('p');
+
+
+
+    let search = document.querySelector('.searchsubmit').addEventListener('click', (e) => {
 
         let searchterm = document.getElementById('searchparameter').value;
 
@@ -21,7 +36,25 @@ export function global() {
 
                 .then
                 ((response) => {
-                    console.log(response);
+                    console.log(response.data);
+
+
+                    document.querySelector('#searchresults').appendChild(createLi);
+                    createLi.appendChild(changeImg);
+                    changeImg.classList.add('poster');
+                    changeImg.src = response.data.poster;
+                    createDiv;
+                    createLi.appendChild(createDiv);
+                    createH3;
+                    createH4Year;
+                    createH4Type;
+                    createDiv.appendChild(createH3);
+                    createDiv.appendChild(createH4Year);
+                    createDiv.appendChild(createH4Type);
+                    createP;
+                    createLi.appendChild(createP)
+
+                    document.querySelector('#resultheader').classList.remove('hidden');
 
                 })
                 .catch((err) => {
---
+++ js/elements.js
@@ -0,0 +1,19 @@
+export function creation() {
+
+    const createLi = document.createElement('li').classList.add('searchres');
+
+    const changeImgSrc = document.getElementsByClassName('poster').src;
+
+    const createDiv = document.createElement('div');
+
+    const createH3 = document.createElement('h3').classList.add('title');
+
+    const createH4Year = document.createElement('h4').classList.add('year');
+
+    const createH4Type = document.createElement('h4').classList.add('type');
+
+    const createP = document.createElement('p').classList.add('plot');
+
+
+
+}
